65aec90629d19ebc0a02253318efd87ac3b92d12
ETEC <164@etec.sp.gov.br>
2025-09-19 15:53:02 +0000

    Aula 18/09/2025

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import application.lista.ListaSimples;
+
 public class Main {
     public static void main(String[] args) {
         // Declaração de Vetores
@@ -7,6 +12,19 @@ public class Main {
         
         // Declaração de Matrizes
         int[][] matriz = new int[5][3];
+
+        // Declaração de Lista Simples
+        ListaSimples lista = new ListaSimples();
+        lista.adicionar("Primeiro");
+        lista.adicionar("Segundo");
+        lista.adicionar("Terceiro");
+        lista.remover(1);
+
+        List<String> lista2 = new ArrayList<String>();
+        lista2.add("Teste");
+        
+
+        System.out.println(lista);
     }
 }
 
